2808f66d9ecdc90cc9b967a9c0c855b30de88f1c
joco66sk <joco66@centrum.sk>
2026-02-26 14:32:19 +0100

    Added site name and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-
+import Head from 'next/head';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Head>
+  <title>Tennis Deep Stats</title>
+  <meta name="description" content="Deep tennis statistics, player analysis, match history, surface splits and more." />
+       </Head>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -14,7 +18,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-           "Welcome to Tennis Stats Site!"
+  Tennis Deep Stats"
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -60,6 +64,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-    </div>
-  );
+  </div>
+);
 }
